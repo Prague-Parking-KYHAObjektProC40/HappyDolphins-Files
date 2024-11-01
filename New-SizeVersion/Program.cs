757ef62acaf512e27bc3b3d3b0300a9be2ee4ee2
Simon Major <42642927+ZabatorXXX@users.noreply.github.com>
2024-11-01 12:08:04 +0100

    Update Program.cs

--- New-SizeVersion/Program.cs
+++ New-SizeVersion/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+[Flags]
+public enum Vehicle
+{
+    None = 0,
+    Bike = 1,
+    Mc = 2,
+    Car = 4,
+    Buss = 8,
+    MC2 = Mc + Mc
+}
+
+public class VehicleInfo
+{
+    public int ID { get; set; }
+    public string Type { get; set; }
+}
+
+public class ParkingApp
+{
+    public static void Main()
+    {
+        // Read vehicles from JSON
+        string json = File.ReadAllText("vehicles.json");
+        var vehicles = JsonConvert.DeserializeObject<Dictionary<string, List<VehicleInfo>>>(json)["Vehicles"];
+
+        foreach (var vehicle in vehicles)
+        {
+            Console.WriteLine($"ID: {vehicle.ID}, Type: {vehicle.Type}");
+        }
+
+        // Use vehicle data as needed
+    }
+}
